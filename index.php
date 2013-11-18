e4a3b4c18ee51b05ba6d1eba17bd5e980d109e5f
Fedor Kurilov <fegorus@gmail.com>
2013-11-18 17:26:36 +0300

    Update index.php

--- index.php
+++ index.php
@@ -16,8 +16,8 @@ if (isset($_POST['code']) && $_POST['code']) {
     <head>
         <meta charset="utf-8">
         <title>Pascal lexical analyzer</title>
-        <link rel="stylesheet" href="css/style.css">
         <link rel="stylesheet" href="css/codemirror.css">
+        <link rel="stylesheet" href="css/style.css">
         <script src="js/jquery-1.10.2.min.js" type="text/javascript"></script>
         <script src="js/codemirror.min.js" type="text/javascript"></script>
         <script src="js/main.js" type="text/javascript"></script>
